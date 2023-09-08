6ba8fe60522582dbd3d1424ef388da1911af81e9
Oluwatobi Moshood <oluwatobimoshood16@gmail.com>
2023-09-08 18:29:55 +0100

    built NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <div className="bg-coral md:block">Nav</div>
+      <div className="bg-coral md:block"><NavBar/></div>
       <div className="grid md:grid-cols-2">
         <div className="bg-coral md:block hidden">Aside</div>
         <div className="bg-dodgerblue">Main</div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { HStack } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
 
-const NavBar = () => (
-    <HStack>
-        
-    </HStack>
-)
+const NavBar = () => {
+    return (
+        <HStack>
+            <img src={logo} className='w-20' alt="" />
+            <p>NavBar</p>
+        </HStack>
+    )
+}
 
 export default NavBar
