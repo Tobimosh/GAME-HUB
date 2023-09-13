a4a1d11e208637c086088d67b9bb0f55693ca366
Oluwatobi Moshood <oluwatobimoshood16@gmail.com>
2023-09-13 23:13:38 +0100

    implemented platform icon

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Game } from "../hooks/useGames";
-import { Card, Image, Heading, CardBody } from "@chakra-ui/react";
+import { Card, Image, Heading, CardBody, Text } from "@chakra-ui/react";
+import PlatformIcon from "./PlatformIcon";
 
 interface Props {
   game: Game;
@@ -10,6 +11,7 @@ export const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        <PlatformIcon platforms={game.parent_platforms.map(p => p.platform)}/>
       </CardBody>
     </Card>
   );
