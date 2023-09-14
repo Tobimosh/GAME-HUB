72f8ebdf9e82d6dd275429b167a673ecbd6f902f
Oluwatobi Moshood <oluwatobimoshood16@gmail.com>
2023-09-14 17:41:14 +0100

    filtered games by platforms

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,19 +1,18 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 
 interface Props {
-  selectedGenre: Genre | null
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-
-const GameGrid = ({selectedGenre}: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -26,12 +25,12 @@ const GameGrid = ({selectedGenre}: Props) => {
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
               {" "}
-              <GameCardSkeleton  />{" "}
+              <GameCardSkeleton />{" "}
             </GameCardContainer>
           ))}
         {data.map((game) => (
           <GameCardContainer key={game.id}>
-            <GameCard  game={game} />
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
