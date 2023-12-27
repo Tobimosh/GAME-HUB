744cc50031a8b6dc585ecbef3c56ff8e89ae74da
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2023-12-27 18:48:09 +0100

    built game screenshots

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import useTrailers from '../hooks/useTrailer'
+import React from "react";
+import useTrailers from "../hooks/useTrailer";
 
-interface Props{
-    gameId: number
+interface Props {
+  gameId: number;
 }
-const GameTrailer = ({gameId}: Props) => {
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
 
-    const {data, error, isLoading} = useTrailers(gameId);
-    if(isLoading) return null;
-    if(error) throw error;
-
-  return (
-        <video src={data?.results[0]?.data[480]}
-        poster={data?.results[0].preview}
+  if (data?.results[0])
+    return (
+      <video
+        src={data.results[0]?.data[480]}
+        poster={data.results[0].preview}
         controls
-        ></video>
-  )
-}
+      ></video>
+    );
+    else{
+        return null;
+    }
+};
 
 export default GameTrailer;
---
+++ src/entities/Screenshot.ts
@@ -0,0 +1,9 @@
+
+
+
+export interface Screenshot{
+     id: number,
+     image: string,
+     width: number,
+     height: number
+}
